078dd36ad22bf87ea56a7a7e2a8330c4ed8a0da8
O-Julia-O <julia.qq.onix@gmail.com>
2025-04-13 16:00:30 +0300

    fix(slider): now the color changing after moving the thumb

--- starter-code/script.js
+++ starter-code/script.js
@@ -8,6 +8,11 @@ const rangBgColor = {
 
 function updateSliderBackground(el) {
     const value = (el.value - el.min) / (el.max - el.min) * 100;
+el.style.background = `linear-gradient(90deg,
+        ${rangBgColor.bgColorA} 0%,
+        ${rangBgColor.bgColorA} ${value}%,
+        ${rangBgColor.bgColorB} ${value}%,
+        ${rangBgColor.bgColorB} 100%)`;
 }
 
 slider.addEventListener('input', function () {
